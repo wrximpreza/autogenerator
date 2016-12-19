e88524f9f09bd0c059ff3c8b382b70046fa06638
wrximpreza <wrx21061987impreza>
2016-12-19 22:53:31 +0200

    add first banner styles

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/banner', banner);
 
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/banner.js
+++ routes/banner.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var app = express();
+var uuid = require('node-uuid');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 router.get('/', function (req, res, next) {
-    console.log('banner');
-    res.render('banner', {title: 'Тест'});
+    res.render('banner', {title: req.param('title'), icon: req.param('icon')});
 });
 
 module.exports = router;
